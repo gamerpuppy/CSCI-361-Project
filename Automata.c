3d8ce04561e274fb6160588a7b33831f943f3766
gamerpuppy <keegan.millard@gmail.com>
2017-10-30 21:52:29 -0600

    cleaned up some excess code

--- Automata.c
+++ Automata.c
@@ -5,8 +5,10 @@
 //  Created by Keegan Millard on 10/30/17.
 //  Copyright © 2017 gamerpuppy. All rights reserved.
 //
-#define MASK_LAST 0x00000001
-#define GENERATION_COUNT 16
+
+#define MASK_LAST 0x1
+#define MASK_TRIPLE 0x7
+#define GENERATION_COUNT 32
 #define RULES 0x7E
 
 #include <stdio.h>
@@ -16,23 +18,19 @@ int generations[GENERATION_COUNT];
 int nextGen(int prevGen) {
     int nextGen = 0;
     for(int i = 1; i < 31; i++) {
-        int left = (prevGen >> (32-i)) & MASK_LAST;
-        int middle = prevGen >> (31-i) & MASK_LAST;
-        int right = prevGen >> (30-i) & MASK_LAST;
-        int result = (RULES >> (left*4+middle*2+right) & MASK_LAST);
+        int neighborhood = (prevGen >> (30-i)) & MASK_TRIPLE;
+        int result = (RULES >> neighborhood & MASK_LAST);
         nextGen += result << i;
     }
     return nextGen;
 
 }
 
-// modifys the input
 void printGens(){
     int curGen = 0;
     while (curGen < GENERATION_COUNT) {
         for(int i = 0; i < 32; i++) {
-            printf("%d", generations[curGen] & MASK_LAST);
-            generations[curGen] = generations[curGen] >> 1;
+            printf("%d", (generations[curGen] >> (31-i)) & MASK_LAST);
         }
         printf("\n");
         curGen++;
