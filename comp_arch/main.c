4e03b82401dac9c453a138f37e3576ef244c9137
gamerpuppy <keegan.millard@gmail.com>
2017-11-27 20:36:07 -0700

    imported final version of project

--- comp_arch/main.c
+++ comp_arch/main.c
@@ -1,33 +1,49 @@
-/* main.c simple program to test assembler program */
+/*
+		Keegan Millard
+		Brett Layman
+		Brandom May
+		~ Team 1
+	*/
 
 #include <stdio.h>
-#define GEN_COUNT 32
-#define MASK_LAST 0x1
-#define INITIAL_VALUE 0x8000000
-#define RULES 0x7E
+#include <stdlib.h>
+#include <string.h>
 
-extern long long int binary_search(long long int sorted[], int, int);
-extern void automata(long long int [], long, long long int, int);
 
-//extern int huffman(FILE * inFp, FILE * outFp);
+
+extern void huff(char[], short[]);
+void printCodes(short[]);
+
+char test[] = "Hello World, this is a placeholder piece of text for "
+		"before I bother writing something long enough to really test the algorithm.";
+
+char test2[] = "EEEEEEE  DFFFFFFFF EEEE HI!";
 
 int main(void) {
-	//code for part 2
-	long long int sorted[15] = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29};
-	long long int value_returned = binary_search(sorted, 15, 29);
-	printf("Result of binary search is %ld\n", value_returned);
 
-	long long int generations[GEN_COUNT];
-	automata(generations, GEN_COUNT * 8, INITIAL_VALUE, RULES);
-	int curGen = 0;
-	while (curGen < GEN_COUNT) {
-		for(int i = 0; i < 64; i++)
-			printf("%lli", (generations[curGen] >> (63-i)) & MASK_LAST);
+	short codes[256];
+	memset(codes, '\0', 256*sizeof(short));	// sets all codes to 0
 
-		printf("\n");
-		curGen++;
-	}
-    return 0;
+	huff(test, codes);		// call assembly
+	printCodes(codes);		// print the huffman codes
+
+	return 0;
+}
+
+void printCodes(short codes[]) {
+	char string [16];
+		for(int i = 0; i < 256; i+=2) {	// code
+			if(codes[i+1] > 0) {// valid codes are all greater than 0 length
+				int z = 0;
+				for (; z < codes[i+1]; z++){
+					int shift = codes[i+1]-z-1;
+					string[z] = codes[i] & (1 << shift) ? '1' : '0';
+				}
+				string[z] = '\0';
+				printf("char: %d codeL: %d code: %s\n", (char)(i/2), codes[i+1], string);
+			}
+		}
+		return;
 }
 
 
